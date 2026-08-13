da970212b7f7ef704cad1e59bd4912408954b0fa
Jati Agung <jatiagung.idn@gmail.com>
2026-08-13 21:21:13 +0700

    chore(db): initialize typescript project and core inventory schema

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import process from "node:process";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
