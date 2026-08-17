04565e7108a6e9d6b93ded062ab32810929ee751
Jati Agung <jatiagung.idn@gmail.com>
2026-08-17 16:17:05 +0700

    refactor(repositories): improve product repository

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -5,36 +5,35 @@ export interface Product {
   sku: string;
   name: string;
   description?: string | null;
-  created_at: string;
-  updated_at: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export async function findAllProducts(): Promise<Product[]> {
-  const result = await pool.query<Product>(
-    `
-        SELECT
-            id,
-            sku,
-            name,
-            description,
-            created_at,
-            updated_at
-        FROM products
-        ORDER BY created_at DESC
-        `,
-  );
+  const query = `
+  SELECT 
+    id,
+    name,
+    sku,
+    description,
+    created_at,
+    updated_at
+  FROM products;
+  `;
+
+  const result = await pool.query<Product>(query);
 
   return result.rows;
 }
 
-export async function getProductById(id: string) {
+export async function getProductById(id: string): Promise<Product | undefined> {
   const query = `
     SELECT id, sku, name, description, created_at, updated_at
     FROM products
-    WHERE id = $1
+    WHERE id = $1;
   `;
 
-  const result = await pool.query(query, [id]);
+  const result = await pool.query<Product>(query, [id]);
   return result.rows[0];
 }
 
@@ -46,7 +45,7 @@ export async function addProduct(
   const query = `
     INSERT INTO products (sku, name, description) 
     VALUES ($1, $2, $3) 
-    RETURNING id, sku, name, description, created_at, updated_at
+    RETURNING id, sku, name, description, created_at, updated_at;
   `;
 
   const result = await pool.query<Product>(query, [
@@ -63,29 +62,48 @@ export async function updateProductById(
   sku: string,
   name: string,
   description: string,
-) {
+): Promise<Product | undefined> {
   const query = `
     UPDATE products
     SET 
-      sku = $1
-      name = $2
-      description = $3
+      sku = $1,
+      name = $2,
+      description = $3,
       updated_at = NOW()
     WHERE id = $4
-    RETURNING id, sku, name, description, created_at, updated_at
+    RETURNING id, sku, name, description, created_at, updated_at;
   `;
 
-  const result = await pool.query(query, [sku, name, description, id]);
+  const result = await pool.query<Product>(query, [sku, name, description, id]);
   return result.rows[0];
 }
 
-export async function deleteProductById(id: string) {
+export async function patchProductById(
+  id: string,
+  sku?: string,
+  name?: string,
+  description?: string,
+): Promise<Product | undefined> {
+  const query = `
+    UPDATE products
+    SET
+      sku = COALESCE($1, sku),
+      name = COALESCE($2, name),
+      description = COALESCE($3, description),
+      updated_at = NOW()
+    WHERE id = $4
+    RETURNING id, sku, name, description, created_at, updated_at;
+  `;
+
+  const result = await pool.query<Product>(query, [sku, name, description, id]);
+  return result.rows[0];
+}
+
+export async function deleteProductById(id: string): Promise<void> {
   const query = `
     DELETE FROM products
-    WHERE id = $1
-    RETURNING id
+    WHERE id = $1;
   `;
 
   const result = await pool.query(query, [id]);
-  return result.rows[0];
 }
