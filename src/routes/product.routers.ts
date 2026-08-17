0479506571032ae7c7a867115364c54b880c64b2
Jati Agung <jatiagung.idn@gmail.com>
2026-08-17 16:55:13 +0700

    feat(routes): add patch to product router

--- src/routes/product.routers.ts
+++ src/routes/product.routers.ts
@@ -4,6 +4,7 @@ import {
   getById,
   postProduct,
   putProduct,
+  patchProduct,
   deleteProduct,
 } from "../controllers/product.controller.js";
 
@@ -13,6 +14,7 @@ router.get("/", getProduct);
 router.get("/:id", getById);
 router.post("/", postProduct);
 router.put("/:id", putProduct);
+router.patch("/:id", patchProduct);
 router.delete("/:id", deleteProduct);
 
 export default router;
